fa9d70ff089652ffcd228678770db43465103c81
myafa <31382399+myafa@users.noreply.github.com>
2018-11-16 16:11:28 +0900

    Create codility_2_2_ww.py

---
+++ coding/codility_2_2_ww.py
@@ -0,0 +1,15 @@
+# you can write to stdout for debugging purposes, e.g.
+# print("this is a debug message")
+
+def solution(A):
+    # write your code in Python 3.6
+    temp = {}
+    for i in A:
+        if i in temp.keys():
+            temp[i]+=1
+        else:
+            temp[i]=1
+    for key in temp.keys():
+        if temp[key]%2:
+            result=key
+    return result
