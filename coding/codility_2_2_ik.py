5574744e169c65b03e8f4e42cb3f1003331f0a8c
goat222 <41246153+goat222@users.noreply.github.com>
2018-11-21 18:16:13 +0900

    Update codility_2_2_ik.py

--- coding/codility_2_2_ik.py
+++ coding/codility_2_2_ik.py
@@ -1,1 +1,12 @@
-dictionary
+def solution(A):
+    # write your code in Python 3.6
+    temp = {}
+    for i in A:
+        if i in temp.keys():
+            temp[i]+=1
+        else:
+            temp[i]=1
+    for key in temp.keys():
+        if temp[key]%2:
+            result=key
+    return result
